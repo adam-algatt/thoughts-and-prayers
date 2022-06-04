497e4c0400554eee1fb68ddb6c230e26ff2caf3e
adam algatt <adam.algatt@gmail.com>
2022-06-04 12:57:51 -0700

    working on schemas

--- models/Thought.js
+++ models/Thought.js
@@ -24,3 +24,17 @@ Schema Settings
 Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
  
 */
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: 'Thought text is Required',
+    min: [1, "You are required to input at least one character for a valid thought"],
+    max: 280,
+    created_at: Date.now
+  },
+
+});
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -24,3 +24,24 @@ Schema Settings
 Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
 
 */
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: 'Username is required, please enter a valid username'
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@+\..+/]
+  },
+
+
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User; 
